c4d4a9b8506f7ddb2f6c2fa1147ac24021ca318a
Ekrem Gunes <95303574+ekremguns@users.noreply.github.com>
2022-11-06 14:16:25 +0300

    Add files via upload

---
+++ 4 öğrencinin 3 adet notu.cpp
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/*1. 4 öðrencinin 3 adet notunu (mat, fizik, kimya) giriniz. 
+Ortalamasýný da matris içinde hesaplayýnýz.*/
+int main()
+{
+	float ortalama,notlar[4][3];
+	int i,j;
+	
+	for(i=0;i<4;i++)
+	{
+
+		printf("Ogrenci %d:\n",i+1);
+		float toplam=0;
+		for(j=0;j<3;j++)
+		{
+			tekrarGirsin:
+			if(j==0)
+			printf("matematik ");
+			else if(j==1)
+			printf("fizik ");
+			else if(j==2)
+			printf("kimya ");
+			
+			
+			printf("notunu giriniz:");
+			scanf("%f",&notlar[i][j]);
+			if(notlar[i][j] > 100 || notlar[i][j] <0)
+				goto tekrarGirsin;
+			
+			toplam+=notlar[i][j];	
+		}
+		ortalama=toplam/3;
+		
+		printf("ortalama= %f\n",ortalama);
+		printf("\n");
+	}	
+return 0;
+}
